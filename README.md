50f95c9f12af5d96451417269cfd141c45741b1f
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-04-10 16:52:43 -0500

    resume page code and styling

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sara-Fuller-Full-Stack-Portfolio
 
 things to ask tutor: 
-body background color
+body background color whitespace
 override bootstrap text color for nav bar
-why does about me styling affect them all
+why does about me styling affect them all -- styles are bleeding over all around
--- Sara-Fuller-Full-Stack-Portfolio/src/pages/ResumePage.jsx
+++ Sara-Fuller-Full-Stack-Portfolio/src/pages/ResumePage.jsx
@@ -1,16 +1,44 @@
+import "../styles/Resume.css"
+
 export default function Resume() {
-    return (
-      <div>
-        <h1>Resume</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>
+        You can find a PDF version of my resume <a href="./src/assets/files/Sara-Fuller-Resume-2024.pdf">here</a>.
+      </p>
+
+      <div className="d-inline-flex justify-content-center w-100">
+        <div className="card p-2 mx-2">
+          <div className="card-body">
+            <h2>Frontend Proficiencies</h2>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Bootstrap</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>React</li>
+              <li>Responsive Design</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card p-2 mx-2">
+          <div className="card-body">
+            <h2>Backend Proficiencies</h2>
+            <ul>
+              <li>APIs</li>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>mySQL, Sequelize</li>
+              <li>MongoDB, Mongoose</li>
+              <li>Progressive Web Applications (PWAs)</li>
+              <li>REST APIs</li>
+            </ul>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
