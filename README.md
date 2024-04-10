f8b25649c1b4bcbf1b912980e9c7fd34926fad90
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-04-10 10:53:46 -0500

    styling project cards and adding images

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 things to ask tutor: 
 body background color
 override bootstrap text color for nav bar
+why does about me styling affect them all
--- Sara-Fuller-Full-Stack-Portfolio/src/pages/PortfolioPage.jsx
+++ Sara-Fuller-Full-Stack-Portfolio/src/pages/PortfolioPage.jsx
@@ -1,16 +1,30 @@
+import "../styles/Portfolio.css";
+
 export default function Portfolio() {
-    return (
-      <div>
-        <h1>Portfolio</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+  return (
+    <>
+      <h1>Portfolio</h1>
+      <div className="card1">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        </div>
       </div>
-    );
-  }
-  
+      <div className="card2">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div className="card3">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </>
+  );
+}
