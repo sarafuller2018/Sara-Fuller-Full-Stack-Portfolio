8bcfac4c392003dfc464fc370f3b49f1c8fe1e16
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-04-15 21:35:54 -0500

    fixed file structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../styles/Footer.css";
+
+function Footer() {
+    return (
+        <div className="d-inline-flex justify-content-center w-100 footer">
+        <a href="https://github.com/sarafuller2018"><img src="/GitHub-Mark.png"/></a>
+        <a href="https://www.linkedin.com/in/sara-fuller/"><img src="/LinkedIn.png"/></a>
+        <a href="mailto:sarafuller2018@gmail.com"><img src="/email-clipart.png"/></a>
+       </div>
+    );
+}
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../styles/Header.css";
 function Header() {
     return (
         <header className="header">
-            <img src="../public/Portfolio-Headshot.png"></img>
+            <img src="/Portfolio-Headshot.png"></img>
         </header>
     );
 }
